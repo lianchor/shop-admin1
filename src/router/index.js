c375c9250b0f6417008e4bc6f78f712b367ff524
lianchor <2337279087@qq.com>
2020-10-21 14:03:18 +0800

    完成用户管理和数据统计部分

---
+++ src/router/index.js
@@ -0,0 +1,83 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+
+
+import Home from '../components/Home.vue'
+import Login from '../components/Login.vue'
+import Users from '../components/user/User'
+import Welcome from "../components/Welcome"
+
+
+import Rights from '../components/power/Rights.vue'
+import Roles from '../components/power/Roles.vue'
+
+import Cate from '../components/goods/Cate.vue'
+import Params from '../components/goods/Params.vue'
+
+import GoodsList from '../components/goods/List.vue'
+import Add from '../components/goods/Add.vue'
+
+import Order from '../components/order/Order.vue'
+import Report from '../components/report/Report.vue'
+
+Vue.use(VueRouter)
+
+const routes = [{
+  path: '/',
+  redirect: '/login'
+}, {
+  path: '/login',
+  component: Login
+}, {
+  path: '/home',
+  component: Home,
+  redirect: '/Welcome',
+
+  children: [{
+    path: '/home',
+    component: Welcome
+  }, {
+    path: '/users',
+    component: Users
+  }, {
+    path: '/rights',
+    component: Rights
+  }, {
+    path: '/roles',
+    component: Roles
+  }, {
+    path: '/categories',
+    component: Cate
+  }, {
+    path: '/params',
+    component: Params
+  }, {
+    path: '/goods',
+    component: GoodsList
+  }, {
+    path: '/goods/add',
+    component: Add
+  }, {
+    path: '/orders',
+    component: Order
+  }, {
+    path: '/reports',
+    component: Report
+  }]
+}]
+
+const router = new VueRouter({
+    routes
+  })
+  // 挂载路由导航守卫,to表示将要访问的路径,from表示从哪里来,next是下一个要做的操作 next('/login')强制跳转login
+router.beforeEach((to, from, next) => {
+  // 访问登录页,放行
+  if (to.path === '/login') return next()
+    // 获取token
+  const tokenStr = window.sessionStorage.getItem('token')
+    // 没有token强制跳转到登录页
+  if (!tokenStr) return next('/login')
+  next()
+})
+
+export default router
